a81aa89768a82c14ec795d83f217376a4db269fd
Hamit <hmermerkaya@gmail.com>
2013-10-09 14:59:27 +0200

    ham

---
+++ PhysicsTools/NtupleUtils/src/classes.h
@@ -0,0 +1,8 @@
+#include "PhysicsTools/NtupleUtils/interface/NtupleFactory.h"
+#include "PhysicsTools/NtupleUtils/interface/MCDumper.h"
+#include <iostream>
+ 
+namespace {
+  struct dictionary {
+   };
+}
